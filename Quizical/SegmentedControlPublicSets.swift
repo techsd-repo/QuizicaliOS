a44606e2877b5fc4c3503fe03c337427afa99419
techsd-repo <techlcsd@gmail.com>
2016-06-12 12:50:28 -0400

    Added some tableview cells

--- Quizical/SegmentedControlPublicSets.swift
+++ Quizical/SegmentedControlPublicSets.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+import Firebase
+
+class SegmentedControlPublicSets: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+    
+}
